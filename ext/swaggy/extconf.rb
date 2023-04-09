50d488df6bd710eb375b80eed19fc0a3f5c290ee
Nigel Baillie <nbaillie@degica.com>
2023-04-09 10:55:08 +0900

    add libfyaml dependency

--- ext/swaggy/extconf.rb
+++ ext/swaggy/extconf.rb
@@ -1,5 +1,33 @@
 require 'mkmf'
 
+libfyaml_source = File.expand_path('../../tmp/libfyaml-0.8', __dir__)
+libfyaml_upstream = 'https://github.com/pantoniou/libfyaml/releases/download/v0.8/libfyaml-0.8.tar.gz'
+
+if !File.exist?(libfyaml_source)
+  puts "Downloading libfyaml..."
+  `curl -L #{libfyaml_upstream} | tar xz -C #{File.dirname(libfyaml_source)}`
+end
+
+if !File.exist?(File.expand_path(libfyaml_source, 'build/lib/libfyaml.a'))
+  puts "Building libfyaml from source"
+  `mkdir -p "#{File.expand_path(libfyaml_source, 'build')}"`
+
+  puts "Bootstrapping libfyaml..."
+  `cd #{libfyaml_source} && ./bootstrap.sh`
+
+  puts "Configuring libfyaml..."
+  `cd #{libfyaml_source} && ./configure "--prefix=#{libfyaml_source}/build"`
+
+  puts "Building libfyaml..."
+  `cd #{libfyaml_source} && make install`
+
+  puts "Forcing static link by removing libfyaml.so..."
+  `rm #{libfyaml_source}/build/lib/libfyaml.so*`
+end
+
+$CFLAGS << " -I#{libfyaml_source}/build/include"
+$LDFLAGS << " -L#{libfyaml_source}/build/lib -lfyaml"
+
 extension_name = 'swaggy'
 dir_config(extension_name)
 create_makefile(extension_name)
